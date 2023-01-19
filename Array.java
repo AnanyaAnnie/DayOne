30f069dfb3c5f6cf4ce9ae349b5fe0450edf886f
AnanyaAnnie <ananyabiswal22@gmail.com>
2023-01-19 20:47:28 +0530

    Changes

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String[] args) {
+
+        int [][] myArr = {{1,2,3,4,5},{3,4,7,9,1},{13,56,23,23},{34,12,13}};
+
+        myArr[1][3];
+
+    }
+}
